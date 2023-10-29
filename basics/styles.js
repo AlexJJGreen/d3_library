4edd69e39c13d850fc52d7906587f3b0e04c9ad9
AlexJJGreen <alexjjgreen@gmail.com>
2023-10-29 18:37:31 +0100

    debounce function, deugging

--- basics/styles.js
+++ basics/styles.js
@@ -1,9 +1,19 @@
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm"
 
-document.addEventListener("DOMContentLoaded", (e) => {
+let debounce = (func, wait) => {
+    let timeout;
+    return () => {
+        const context = this, args = arguments;
+        clearTimeout(timeout);
+        timeout = setTimeout(() => {
+            func.apply(context, args);
+        }, wait);
+    };
+}
 
+let d3Graph = (svgEl) => {
     // Select SVG Element, calulate width + coerces value 100px - slice to number
-    let svg = d3.select("#svg");
+    let svg = d3.select(svgEl);
     let svgWidth = svg.node().getBoundingClientRect().width;
 
     // define margins
@@ -37,5 +47,11 @@ document.addEventListener("DOMContentLoaded", (e) => {
     svg.append("g")
         .attr("transform", `translate(${margins.left},${chartHeight + margins.top})`)
         .call(xAxis);
+};
 
+document.addEventListener("DOMContentLoaded", (e) => {
+    // render SVG on dom load, resize on resize with debounce
+    d3Graph("#svg");
+    document.addEventListener("resize", debounce(d3Graph("#svg"), 200));
 });
+
